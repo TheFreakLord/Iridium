ab7f7d235e48be71cffdcb22d126d41c26d6de36
laklaklak <iililiiillliliililillij@gmail.com>
2016-05-01 13:36:09 +0200

    Added minimal spigot stuff.

---
+++ src/main/java/cf/backspace/plugins/iridium/Iridium.java
@@ -0,0 +1,20 @@
+package cf.backspace.plugins.iridium;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by paul on 01.05.16.
+ */
+public class Iridium extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
